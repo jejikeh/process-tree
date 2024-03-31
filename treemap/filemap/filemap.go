c06e668dce8a316d7533719e19402249c45e0698
jejikeh <jejikeh@gmail.com>
2024-04-01 01:29:30 +0300

    misc: Move WalkDir func to separate visitFile func with Treemap pointer as a parameter

--- treemap/filemap/filemap.go
+++ treemap/filemap/filemap.go
@@ -9,21 +9,27 @@ import (
 )
 
 func InitTreemap(path string) (*treemap.Treemap, error) {
+	tree := treemap.NewTreemap()
+
 	if err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if d.IsDir() {
-			fmt.Printf("DIRECTORY: %s\n", d.Name())
-		} else {
-			fmt.Printf("FILE: %s\n", d.Name())
-		}
-
-		return nil
+		return visitFile(path, &tree, d, err)
 	}); err != nil {
 		return nil, err
 	}
 
 	return nil, nil
+}
+
+func visitFile(path string, tree *treemap.Treemap, d fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+
+	if d.IsDir() {
+		fmt.Printf("DIRECTORY: %s\n", filepath.Dir(d.Name()))
+	} else {
+		fmt.Printf("FILE: %s\n", d.Name())
+	}
+
+	return nil
 }
